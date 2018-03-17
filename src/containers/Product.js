2ac864836348f653da0e603c39226e8c7910f9b3
smoothpie <kupriyanovichk72@gmail.com>
2018-03-17 18:29:13 -0400

    Added new products

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -2,7 +2,20 @@ import React from 'react'
 import { withRouteData, Link } from 'react-static'
 
 export default withRouteData(({ product }) => {
-    if (product.id == 1 || product.id == 2) {
+  if (product.id == 1) {
+    return (
+      <div className="product-page">
+        <div className="product_images">
+          <img src={require(`../../public/images/products/${product.image}`)}/>
+        </div>
+        <div className="product_description">
+          <h1>{product.title}</h1>
+          <p>Принт с 1-ой стороны - 29 р. С 2-x - 35 р.</p>
+          <p>Размерный ряд: S - XXL.</p>
+          <p>Присылайте свои макеты нам на почту :)</p>
+        </div>
+      </div>
+  )} else if (product.id == 10 || product.id == 11) {
     return (
       <div className="product-page">
         <div className="product_images">
@@ -18,38 +31,36 @@ export default withRouteData(({ product }) => {
           </Link>
         </div>
       </div>
-    )} else {
-
-  if (product.category === "tshirt") {
-    return (
-      <div className="product-page">
-        <div className="product_images">
-          <img src={require(`../../public/images/products/${product.image}`)} />
+    )} else if (product.category === "tshirt") {
+      return (
+        <div className="product-page">
+          <div className="product_images">
+            <img src={require(`../../public/images/products/${product.image}`)} />
+          </div>
+          <div className="product_description">
+            <h1>{product.title}</h1>
+            <p>Стоимость: {product.price}</p>
+            <p>Размерный ряд: S - XXL.</p>
+            <Link to={`/product/${product.id}/order`} style={{ fontFamily: 'Comfortaa' }}>
+              <button className="order-button">Заказать</button>
+            </Link>
+          </div>
         </div>
-        <div className="product_description">
-          <h1>{product.title}</h1>
-          <p>Стоимость: {product.price}</p>
-          <p>Размерный ряд: S - XXL.</p>
-          <Link to={`/product/${product.id}/order`} style={{ fontFamily: 'Comfortaa' }}>
-            <button className="order-button">Заказать</button>
-          </Link>
+      )
+    } else {
+      return (
+        <div className="product-page">
+          <div className="product_images">
+            <img src={require(`../../public/images/products/${product.image}`)}/>
+          </div>
+          <div className="product_description">
+            <h1>{product.title}</h1>
+            <p>Стоимость: {product.price}</p>
+            <Link to={`/product/${product.id}/order`} style={{ fontFamily: 'Comfortaa' }}>
+              <button className="order-button">Заказать</button>
+            </Link>
+          </div>
         </div>
-      </div>
-    )
-  } else {
-    return (
-      <div className="product-page">
-        <div className="product_images">
-          <img src={require(`../../public/images/products/${product.image}`)}/>
-        </div>
-        <div className="product_description">
-          <h1>{product.title}</h1>
-          <p>Стоимость: {product.price}</p>
-          <Link to={`/product/${product.id}/order`} style={{ fontFamily: 'Comfortaa' }}>
-            <button className="order-button">Заказать</button>
-          </Link>
-        </div>
-      </div>
-    )
-  }}
-})
+      )
+    }
+                            })
